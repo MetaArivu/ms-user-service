5e1e469368d058583118b7c4c692046e287f11a0
ketangote <ketan.gote@metamagic.in>
2021-12-06 13:55:28 +0530

    Changes: Updated Cross Origin..

--- src/main/java/com/user/adapter/controller/v1/UserController.java
+++ src/main/java/com/user/adapter/controller/v1/UserController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(V1)
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 	private static final Logger log = (Logger) LoggerFactory.getLogger(UserController.class);
