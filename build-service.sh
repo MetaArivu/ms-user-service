35d5fdcd0c404cc1a801874e7ff700660e565bda
deepali-arvind <deepali.arvind@metamagic.in>
2021-12-30 13:53:12 +0530

    Update build-service.sh

--- build-service.sh
+++ build-service.sh
@@ -4,6 +4,6 @@
 # -------------------------------------------------------------------------------------------
 mvn clean package
 
-cp -rf target/user-service-1.0.jar Docker/
+cp -rf target/*.jar Docker/
 
 
